2332c973e14b37d7f008033a21c38cbe3b7af3f3
William Nakashima <102883025+William-Nks@users.noreply.github.com>
2022-07-17 20:25:29 +0100

    only folders

---
+++ middleware/loggin_checker.js
@@ -0,0 +1,15 @@
+module.exports.policy = (rule={}) => {
+    
+    return (req, res, next) => {
+        
+        const {access} = rule
+
+        if(!req.session.user && access == 'auth')
+            res.redirect('/login')
+
+        if (req.session.user && access == 'visitor')
+            res.redirect('/index')
+
+        next()
+    }
+}
